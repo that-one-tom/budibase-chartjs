9d1cc44a05cc868417a6710aff22702bdc64f423
that-one-tom <19203795+that-one-tom@users.noreply.github.com>
2022-10-30 12:31:09 +0100

    Added support for chart options

--- src/Component.svelte
+++ src/Component.svelte
@@ -5,11 +5,12 @@
   import 'chart.js/auto';
 
   export let chartdata;
+  export let chartoptions;
   
   const { styleable } = getContext("sdk");
   const component = getContext("component");
 </script>
 
 <div use:styleable={$component.styles}>
-  <Line data={ typeof chartdata === 'string' ? JSON.parse(chartdata) : chartdata } options={{ responsive: true }} />
+  <Line data={ typeof chartdata === 'string' ? JSON.parse(chartdata) : chartdata } options={Object.assign({ responsive: true }, typeof chartoptions === 'string' ? JSON.parse(chartoptions) : chartoptions)} />
 </div>
